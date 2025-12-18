0e8c0c83383a539549f28acbe0a53b91c60cd238
merescalante <meredithescalante@unitec.edu>
2025-12-18 15:46:24 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+	id: 'com.app',
+	root: '#app',
+	theme: 'md',
+	view: {
+		pushState: true
+	},
+	cache: false,
+	cacheDuration: 0,
+	modalTitle: 'Order',
+	panel: {
+		swipe: true,
+	},
+	routes: [
+
+		{
+			path: '/home/',
+			url: 'index.html',
+			name: 'home',
+		},
+		{
+			path: '/categories/',
+			url: 'categories.html',
+			name: 'categories',
+		},
+		{
+			path: '/cart/',
+			url: 'cart.html',
+			name: 'cart',
+		},
+	],
+	popup: {
+		closeOnEscape: true,
+		backdrop: false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
